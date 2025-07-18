d417c5b396611bf669d211e133dc067d7f83db46
chainsawriot <chainsawtiney@gmail.com>
2025-07-18 16:08:06 +0200

    Fix #12

--- lib.R
+++ lib.R
@@ -18,21 +18,6 @@ read_text_base <- function(input_path, dvsep, docvarnames) {
     return(output)
 }
 
-## To prove that the corpora produced are functionally the same (except all the quanteda metadata)
-
-## x <- quanteda::corpus(read_text_base(here::here("rawdata/jankin/TXT/"),
-##                                      dvsep = "_", 
-##                                      docvarnames = c("Country", "Session", "Year")))
-
-## ungd_files <- readtext::readtext(here::here("rawdata/jankin/TXT/*"), 
-##                                  docvarsfrom = "filenames", 
-##                                  dvsep="_", 
-##                                  docvarnames = c("Country", "Session", "Year"))
-## y <- quanteda::corpus(ungd_files)
-
-## for (i in sample(seq_len(quanteda::ndoc(x)), 100)) {
-##     testthat::expect_equal(x[i], y[i])
-## }
 
 
 #' A replacement of textstem::lemmatize_words
@@ -44,33 +29,3 @@ lemmatize_words <- function(tokens) {
     return(tokens)
 }
 
-## To prove that the lemmatizations are the same
-
-## ungd_files <- readtext::readtext(
-##     here::here("rawdata/jankin/TXT/"),
-##     dvsep = "_",
-##     docvarnames = c("Country", "Session", "Year")
-## )
-
-## ungd_files$doc_id <- stringr::str_replace(ungd_files$doc_id, ".txt", "") |>
-##     stringr::str_replace("_\\d{2}", "")
-
-## ungd_corpus <- quanteda::corpus(ungd_files, text_field = "text")
-
-## ungd_tokens <- quanteda::tokens(
-##     ungd_corpus,
-##     what = "word",
-##     remove_punct = TRUE,
-##     remove_symbols = TRUE,
-##     remove_numbers = TRUE,
-##     remove_url = TRUE,
-##     split_hyphens = FALSE,
-##     verbose = TRUE
-## ) |>
-##     quanteda::tokens_tolower()
-
-## ori_types <- attr(ungd_tokens, "types")
-## textstem_lemmatized <- textstem::lemmatize_words(ori_types)
-## our_lemmatized  <- lemmatize_words(ori_types)
-
-## testthat::expect_identical(textstem_lemmatized, our_lemmatized)

7a3a542f33e196a63bf08ee9a7254e8e6cc3ca86
chainsawriot <chainsawtiney@gmail.com>
2025-07-21 16:00:35 +0200

    intermediate

--- czymara01_read.R
+++ czymara01_read.R
@@ -1,9 +1,8 @@
 library(here)
 library(quanteda)
-
+library(haven)
+library(udpipe)
 ## foreign::read.dta doesn't work
-
-
 input <- haven::read_dta(here("rawdata/Corona-Survey_full.dta"))
 
 ## The lost art of doing data manipulation with base
@@ -11,9 +10,14 @@ input$gender <- NA
 input$gender[input$DE03 == 1] <- "male"
 input$gender[input$DE03 == 2] <- "female"
 
-data_priv <- input[ which(input$OF01_01 != ""), ]
+data_priv <- input[ which(input$OF01_01 != "") | which(input$OF01_01 != " "), ]
 
 corpus_priv <- corpus(as.character(data_priv$OF01_01),
                       docvars = data.frame(gender = data_priv$gender,
                                            id = data_priv$CASE))
 
+german_model <- udpipe_load_model(file = here::here("rawdata/german-gsd-ud-2.5-191206.udpipe"))
+
+parsed_content <- udpipe_annotate(german_model, corpus_priv)
+
+parsed_content_df <- as.data.frame(parsed_content)

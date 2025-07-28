92857e8673cf2bfac3f32af7b79b44ab660fcb8d
chainsawriot <chainsawtiney@gmail.com>
2025-07-28 16:30:15 +0200

    Update hook

--- tests/precommit.sh
+++ tests/precommit.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 STAGED_FILES=$(git diff --cached --name-only)
+NEED_RERENDERING=false
 
-if echo "$STAGED_FILES" | grep -q "renv.lock"; then
-  if ! make rmd; then
-    echo "Error: 'make rmd' failed. Aborting commit."
-    exit 1 # Exit with a non-zero status to abort the commit
-  fi
+for FILE in $STAGED_FILES
+do
+    if [[ "$FILE" == "renv.lock" || "$FILE" == "readme.rmd" ]]; then
+	$NEED_RERENDERING=true
+	break
+    fi
+done
+
+if $NEED_RERENDERING; then
+    if ! make rmd; then
+	echo "Error: 'make rmd' failed. Aborting commit."
+	exit 1 # Exit with a non-zero status to abort the commit
+    fi
+    git add readme.md
+    exit 0
+else
+    exit 0
 fi
-
-git add readme.md
-exit 0

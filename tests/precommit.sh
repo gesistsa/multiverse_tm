0efb789183a523cc1c773c74f680826b8817b4c8
chainsawriot <chainsawtiney@gmail.com>
2025-07-28 11:37:26 -0700

    Fix #35 (#36)
    Fix #35 (add pre-commit hook)

---
+++ tests/precommit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+STAGED_FILES=$(git diff --cached --name-only)
+NEED_RERENDERING=false
+
+for FILE in $STAGED_FILES
+do
+    if [[ "$FILE" == "renv.lock" || "$FILE" == "readme.rmd" ]]; then
+	NEED_RERENDERING=true
+	break
+    fi
+done
+
+if $NEED_RERENDERING; then
+    if ! make rmd; then
+	echo "Error: 'make rmd' failed. Aborting commit."
+	exit 1
+    fi
+    git add readme.md
+    exit 0
+else
+    exit 0
+fi

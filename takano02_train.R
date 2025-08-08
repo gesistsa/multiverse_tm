530b1b3783e56797293f6c571b1bbf9978a23e92
chainsawriot <chainsawtiney@gmail.com>
2025-08-08 18:51:25 +0200

    Add tests for 02

--- takano02_train.R
+++ takano02_train.R
@@ -87,3 +87,40 @@ train_model <- function(setting, args, .fix_seed = NULL, .return_output = FALSE)
 purrr::walk(settings, train_model,
             args = args,
             .progress = !args$debug)
+
+if (args$debug) {
+    library(testthat)
+    for (setting in settings) {
+        current_hash <- rlang::hash(setting)
+        testthat::expect_true(file.exists(file.path(args$output_dir, paste0(current_hash, ".RDS"))))
+        output <- readRDS(file.path(args$output_dir, paste0(current_hash, ".RDS")))
+        if (setting$alternative_model) {
+            testthat::expect_true("keyATM_output" %in% class(output$mod))
+        } else {
+            testthat::expect_true("STM" %in% class(output$mod))
+        }
+        expected_k <- c(7, 6, 8)[setting$k_setting]
+        expect_equal(expected_k, ncol(output$theta))
+        ## can't test iter
+        ## output$theta
+        dfm_filename <- paste0(rlang::hash(setting[1:3]), ".RDS")
+        current_dfm <- readRDS(here(args$prefix, args$slug, dfm_filename))
+        expect_equal(ndoc(current_dfm), nrow(output$theta))
+        expect_equal(ndoc(current_dfm), nrow(output$theta))
+        expect_equal(expected_k, ncol(output$theta))
+        cat("Reproducibility check \n")
+        repro_settings <- sample(settings, 2)
+        for (setting in repro_settings) {
+            current_hash <- rlang::hash(setting)
+            testthat::expect_true(file.exists(file.path(args$output_dir, paste0(current_hash, ".RDS"))))
+            output <- readRDS(file.path(args$output_dir, paste0(current_hash, ".RDS")))
+            print("seed:")
+            print(output$random_seed)
+            new_output <- train_model(setting,
+                                      args = args,
+                                      .fix_seed = output$random_seed,
+                                      .return_output = TRUE)
+            testthat::expect_equal(output$mod$theta[,1], new_output$mod$theta[,1])
+        }
+    }
+}

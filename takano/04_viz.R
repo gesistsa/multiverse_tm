97a03c3d52f5acbd7b9e4d1d4f7bbbd2b8fee894
chainsawriot <chainsawtiney@gmail.com>
2025-08-20 15:17:07 +0200

    Update takano tvinnereim

--- takano/04_viz.R
+++ takano/04_viz.R
@@ -2,10 +2,10 @@ library(here)
 
 slug <- "takano"
 
-results <- read.csv(here("results", "aggregated", slug, "1.csv"))
-tmmv.plot_spec_curve(results, tmmv.data[[slug]])
+read.csv(here("results", "aggregated", slug, "1.csv")) |>
+    tmmv.plot_spec_curve(tmmv.data[[slug]])
 
-results <- purrr::map(1:3, \(x) {
+purrr::map(1:3, \(x) {
     read.csv(here("results", "aggregated", slug, paste0(x, ".csv")))
-})
-tmmv.plot_spec_curve(results, tmmv.data[[slug]])
+}) |>
+    tmmv.plot_spec_curve(tmmv.data[[slug]])

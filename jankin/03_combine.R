833aef2f35c276556bd7aa8e0a732f2e3cbcad8c
chainsawriot <chainsawtiney@gmail.com>
2025-08-27 14:04:30 +0200

    Jankin

--- jankin/03_combine.R
+++ jankin/03_combine.R
@@ -2,28 +2,20 @@ args <- tmmv.parse_args_train(slug = "jankin")
 settings <- tmmv.get_settings(full = TRUE)
 
 library(here)
-library(keyATM)
-library(quanteda)
-library(SnowballC)
-library(seededlda)
-library(furrr)
-
-get_theta <- function(setting, args) {
-    current_mod <- readRDS(here(
-        args$output_dir,
-        paste0(rlang::hash(setting), ".RDS")
-    ))
-    current_mod$theta
-}
+## library(keyATM)
+## library(quanteda)
+## library(SnowballC)
+## library(seededlda)
+## library(furrr)
 
 output <- list()
 
 for (setting in settings) {
     hash <- rlang::hash(setting)
-    print(hash)
-    current_mod <- readRDS(here(args$output_dir, paste0(hash, ".RDS")))
+    current_mod <- readRDS(tmmv.get_rds_filename(setting, args$output_dir))
     output[[hash]] <- current_mod$mod$theta
 }
+
 output_path <- here(
     "intermediate",
     args$slug,
@@ -32,4 +24,5 @@ output_path <- here(
     "theta",
     "theta.RDS"
 )
+
 saveRDS(output, output_path)

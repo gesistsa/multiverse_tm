6aee9f974f46c2096afcd2f0d3a63056bf5a048a
chainsawriot <chainsawtiney@gmail.com>
2025-08-27 16:57:19 +0200

    Fix #90

--- curini/03_combine.R
+++ curini/03_combine.R
@@ -12,6 +12,8 @@ library(here)
 
 settings <- tmmv.get_settings(full = TRUE)
 
+tmmv.create_dir(args, ontop = "theta")
+
 get_theta_by_topic_name <- function(topic_name, mod) {
     topic_index <- which(stringr::str_detect(
         colnames(mod$mod$theta),
@@ -118,6 +120,10 @@ cbind(
 
 
 hashes <- purrr::map_chr(settings, \(x) rlang::hash(x))
+
+theta <- purrr::map(res, \(x) x$data$multi100)
+names(theta) <- hashes
+saveRDS(theta, fs::path(args$output_dir, "theta", "theta.RDS"))
 
 generate_conditional_effect <- function(res, hash) {
     .f = function(x, mod, data) {

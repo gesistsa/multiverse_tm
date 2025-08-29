22215cfec7a84d77ef0094e34291ca771e73cc79
chainsawriot <chainsawtiney@gmail.com>
2025-08-30 00:37:52 +0200

    Fix #102

--- lib/lib.R
+++ lib/lib.R
@@ -383,12 +383,24 @@ tmmv.cache_requirements <- function() {
 
 ## reading theta generated via 03_combine.R scripts (or 02a_theta.R for chan)
 tmmv.read_thetas <- function(slug, runs = c(1, 2, 3)) {
+    .f2 <- function(x) {
+        output <- x[, which(stringr::str_detect(colnames(x), "SDG10"))]
+        names(output) <- NULL
+        return(output)
+    }
     .f <- function(run, slug) {
         path <- here::here("intermediate", slug, "runs", run, "theta/theta.RDS")
         if (fs::file_exists(path)) {
-            return(readRDS(path))
+            if (slug != "jankin") {
+                return(readRDS(path))
+            } else {
+                content <- readRDS(path)
+                output <- purrr::map(content, .f2)
+                names(output) <- names(content)
+                return(output)
+            }
         }
         NULL
     }
-    purrr::map(runs, .f = .f, slug = slug)
+    purrr::map(runs, .f = .f, slug = slug) |> purrr::discard(is.null)
 }
---
+++ meta/experimental_factor.R
@@ -0,0 +1,32 @@
+library(here)
+library(dplyr)
+library(ggplot2)
+
+## because takano doesn't have the same size
+## we weight it so that the max eigenvalue is comparable
+calculate_pca <- function(slug, multiverse_size = 216) {
+    .f <- function(run, thetas, slug, multiverse_size = 216) {
+        df <- as.data.frame(thetas[[run]])
+        pca <- prcomp(df, scale = TRUE)
+        weight <- multiverse_size / ncol(df)
+        eigenvalues <- (pca$sdev^2) * weight
+        data.frame(
+            slug = slug,
+            run = run,
+            i = seq_along(eigenvalues),
+            eigenvalue = round(eigenvalues, 5)
+        )
+    }
+    thetas <- tmmv.read_thetas(slug)
+    purrr::map(seq_along(thetas), .f = .f, thetas = thetas, slug = slug) |>
+        purrr::list_rbind()
+}
+
+combined_pca <- purrr::map(names(tmmv.data), calculate_pca) |>
+    purrr::list_rbind()
+
+dplyr::filter(combined_pca, i <= 10) |>
+    ggplot(aes(x = i, y = eigenvalue, color = slug)) +
+    geom_line(linewidth = 1) +
+    scale_color_manual(values = as.character(tmmv.palette_safe)) +
+    facet_grid(cols = vars(run))

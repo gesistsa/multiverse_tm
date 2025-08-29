ee438537b78395f7e19ff4025225c38035052223
chainsawriot <chainsawtiney@gmail.com>
2025-08-29 09:19:53 +0200

    Minor tweak

--- lib/calculate_icc.R
+++ lib/calculate_icc.R
@@ -1,7 +1,7 @@
 # Modified from William Revelle (2025). psych: Procedures for Psychological, Psychometric, and Personality Research. Northwestern University, Evanston, Illinois. R package version 2.5.6, https://CRAN.R-project.org/package=psych.
 # Original license: GPL >= 2
 tmmv.calculate_icc <- function(x) {
-    ## please note that x can be named list
+    ## please note that x can't be named list
     if (length(unique(x)) == 1) {
         ## all the same, usually the case for STM with spectral initialization
         return(1)
@@ -9,11 +9,14 @@ tmmv.calculate_icc <- function(x) {
     x <- purrr::map(x, as.data.frame) |>
         purrr::quietly(purrr::list_cbind)() |>
         purrr::chuck("result")
-    ## most of the code below was copied from psych::ICC version 2.5.6
+    ## most of the code below was copied and modified from psych::ICC version 2.5.6
     n.obs <- nrow(x)
     nj <- ncol(x)
     x.s <- stack(x)
-    x.df <- data.frame(x.s, subs = rep(paste("S", 1:n.obs, sep = ""), nj))
+    x.df <- data.frame(
+        x.s,
+        subs = rep(paste("S", seq_len(n.obs), sep = ""), nj)
+    )
     colnames(x.df) <- c("values", "items", "id")
     quiet_lmer <- purrr::quietly(lme4::lmer)
     mod.lmer <- quiet_lmer(

e1df46cd1e5af27e113bd5e1f1b9de74eba5e443
chainsawriot <chainsawtiney@gmail.com>
2025-08-30 13:17:01 +0200

    Improve readability

--- dev/optimal_transport.R
+++ dev/optimal_transport.R
@@ -3,31 +3,29 @@ library(transport)
 df_theta1 <- readRDS(here("dev", "1 1.RDS"))
 df_theta2 <- readRDS(here("dev", "2 1.RDS"))
 
-rho <- matrix(data = 0, nrow = ncol(df_theta1), ncol = ncol(df_theta2))
+cost_matrix <- matrix(data = 0, nrow = ncol(df_theta1), ncol = ncol(df_theta2))
 
 for (i in seq_len(ncol(df_theta1))) {
     for (j in seq_len(ncol(df_theta2))) {
-        rho[i, j] <- cor(df_theta1[, i], df_theta2[, j], method = "spearman")
+        cost_matrix[i, j] <- (1 -
+            cor(df_theta1[, i], df_theta2[, j], method = "spearman")) /
+            2
     }
 }
 
-C <- (1 - rho) / 2
-
-i <- 1
-
-
 res <- purrr::map_dbl(seq_len(nrow(df_theta1)), \(x) {
     sum(
-        transport(
+        transport::transport(
             df_theta1[x, ],
             df_theta2[x, ],
-            costm = C,
+            costm = cost_matrix,
             fullreturn = TRUE
         )$primal *
-            C
+            cost_matrix
     )
 })
 
-sum(res)
-
-sum(res) / nrow(df_theta1)
+## total cost
+testthat::expect_equal(sum(res), 704.82100777)
+## avg cost
+testthat::expect_equal(sum(res) / nrow(df_theta1), 0.3332487)

09e2cbce8f1ec12c1170d06feba8ae983db8119c
chainsawriot <chainsawtiney@gmail.com>
2025-09-01 11:36:20 +0200

    Edge

--- dev/transfer_cost.R
+++ dev/transfer_cost.R
@@ -8,7 +8,6 @@ run <- 1
 
 settings <- tmmv.get_settings(full = TRUE)
 
-args <- list()
 args$output_dir <- here("intermediate", slug, "runs", run)
 args$debug <- FALSE
 
@@ -21,15 +20,20 @@ allcombis_list <- purrr::map(seq_len(nrow(allcombis)), \(x) {
     allcombis[x, , drop = TRUE]
 })
 
-calculate_cost <- function(combi, settings, args) {
-    thetaa <- readRDS(tmmv.get_rds_filename(
-        settings[[combi[1]]],
+all_thetas <- purrr::map(settings, \(x) {
+    readRDS(tmmv.get_rds_filename(
+        x,
         args$output_dir
     ))$theta
-    thetab <- readRDS(tmmv.get_rds_filename(
-        settings[[combi[2]]],
-        args$output_dir
-    ))$theta
+})
+
+all_hashes <- purrr::map_chr(settings, rlang::hash)
+names(all_thetas) <- all_hashes
+
+
+calculate_cost <- function(combi, all_thetas, all_hashes) {
+    thetaa <- all_thetas[[all_hashes[combi[1]]]]
+    thetab <- all_thetas[[all_hashes[combi[2]]]]
     tmmv.calculate_optimal_transport_cost(thetaa, thetab)
 }
 
@@ -43,8 +47,49 @@ print(Sys.time())
 output <- furrr::future_map_dbl(
     allcombis_list,
     calculate_cost,
+    all_thetas = all_thetas,
+    all_hashes = all_hashes,
+    .progress = TRUE
+)
+print(Sys.time())
+
+saveRDS(output, "dev/tvinnereim.RDS")
+
+args$output_dir <- here("intermediate", "czymara", "runs", run)
+
+print(Sys.time())
+output <- furrr::future_map_dbl(
+    allcombis_list,
+    calculate_cost,
     settings = settings,
     args = args,
     .progress = TRUE
 )
 print(Sys.time())
+
+saveRDS(output, "dev/czymara.RDS")
+hist(readRDS("dev/tvinnereim.RDS"))
+
+mean(readRDS("dev/czymara.RDS"))
+mean(readRDS("dev/tvinnereim.RDS"))
+
+
+node_i <- purrr::map_int(allcombis_list, 1)
+node_j <- purrr::map_int(allcombis_list, 2)
+
+weight <- readRDS("dev/tvinnereim.RDS")
+
+library(igraph)
+library(dplyr)
+
+g <- graph_from_data_frame(data.frame(node_i, node_j, weight), directed = FALSE)
+
+eccentricity(g)
+
+## the fact that the graph is fully connected, make these two the same: two nodes must be connected by just one edge
+node_n <- 2
+data.frame(node_i, node_j, weight) |>
+    dplyr::filter(node_i == node_n | node_j == node_n) |>
+    select(weight) |>
+    max()
+eccentricity(g)[node_n]
